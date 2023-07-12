e354789b5afc76e1c0879557b1dd83030da43eeb
filip <fbielejec@gmail.com>
2023-07-12 12:31:44 +0200

    use westend as relay

--- scripts/zombienet_setup.sh
+++ scripts/zombienet_setup.sh
@@ -7,14 +7,17 @@
 cd local-network
 
 # Download zombienet binary and make it executable
-wget -nc https://github.com/paritytech/zombienet/releases/download/v1.3.39/zombienet-linux-x64
-chmod +x zombienet-linux-x64
-
+zombienet_binary=zombienet-linux-x64
+echo "zombienet_binary" $zombienet_binary
+wget -nc https://github.com/paritytech/zombienet/releases/download/v1.3.39/$zombienet_binary
+chmod +x $zombienet_binary
 
 # Setup zombienet: prepare polkadot binary.
-wget -nc https://github.com/paritytech/polkadot/releases/download/v0.9.40/polkadot
-chmod +x polkadot
+polkadot_binary=polkadot
+echo "polkadot_binary" $polkadot_binary
+wget -nc https://github.com/paritytech/polkadot/releases/download/v0.9.40/$polkadot_binary
+chmod +x $polkadot_binary
 
 cd ..
 
-docker build . -t aleph-parachain-node
+# docker build . -t aleph-parachain-node
