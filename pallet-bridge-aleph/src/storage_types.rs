dcb6dfafcdb174af9dbf8ed37127fd1dacde6526
Paweł Obrok <pawel.obrok@cardinals.cc>
2023-08-23 16:03:56 +0200

    Apply formatting rules from aleph-node

--- pallet-bridge-aleph/src/storage_types.rs
+++ pallet-bridge-aleph/src/storage_types.rs
@@ -17,51 +17,52 @@
 
 //! Wrappers for public types that are implementing `MaxEncodedLen`
 
-use crate::{Config, Error};
-
 use bp_aleph_header_chain::{AuthorityId, AuthoritySet, ChainWithAleph};
 use codec::{Decode, Encode, MaxEncodedLen};
 use frame_support::{traits::Get, BoundedVec, RuntimeDebugNoBound};
 use scale_info::TypeInfo;
-
 use sp_std::marker::PhantomData;
+
+use crate::{Config, Error};
 
 pub struct MaxAuthoritiesCount<T: Config>(PhantomData<T>);
 
 impl<T: Config> Get<u32> for MaxAuthoritiesCount<T> {
-	fn get() -> u32 {
-		T::BridgedChain::MAX_AUTHORITIES_COUNT
-	}
+    fn get() -> u32 {
+        T::BridgedChain::MAX_AUTHORITIES_COUNT
+    }
 }
 
 /// Bounded AlephBFT Authority Set.
 #[derive(Clone, Decode, Encode, TypeInfo, MaxEncodedLen, RuntimeDebugNoBound)]
 #[scale_info(skip_type_params(T))]
 pub struct StoredAuthoritySet<T: Config> {
-	/// List of AlephBFT authorities for the current session.
-	pub authorities: BoundedVec<AuthorityId, MaxAuthoritiesCount<T>>,
+    /// List of AlephBFT authorities for the current session.
+    pub authorities: BoundedVec<AuthorityId, MaxAuthoritiesCount<T>>,
 }
 
 impl<T: Config> StoredAuthoritySet<T> {
-	/// Try to create a new bounded AlephBFT Authority Set from unbounded list.
-	///
-	/// Returns error if number of authorities in the provided list is too large.
-	pub fn try_new(authorities: AuthoritySet) -> Result<Self, Error<T>> {
-		Ok(Self {
-			authorities: TryFrom::try_from(authorities)
-				.map_err(|_| Error::TooManyAuthoritiesInSet)?,
-		})
-	}
+    /// Try to create a new bounded AlephBFT Authority Set from unbounded list.
+    ///
+    /// Returns error if number of authorities in the provided list is too large.
+    pub fn try_new(authorities: AuthoritySet) -> Result<Self, Error<T>> {
+        Ok(Self {
+            authorities: TryFrom::try_from(authorities)
+                .map_err(|_| Error::TooManyAuthoritiesInSet)?,
+        })
+    }
 }
 
 impl<T: Config> Default for StoredAuthoritySet<T> {
-	fn default() -> Self {
-		Self { authorities: BoundedVec::default() }
-	}
+    fn default() -> Self {
+        Self {
+            authorities: BoundedVec::default(),
+        }
+    }
 }
 
 impl<T: Config> From<StoredAuthoritySet<T>> for AuthoritySet {
-	fn from(t: StoredAuthoritySet<T>) -> Self {
-		t.authorities.into()
-	}
+    fn from(t: StoredAuthoritySet<T>) -> Self {
+        t.authorities.into()
+    }
 }
